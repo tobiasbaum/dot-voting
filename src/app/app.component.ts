9cf139e5f32936097e1d59b3d2e913f833c56ed8
Tobias Baum <tbaum@tntinteractive.de>
2021-04-18 22:01:30 +0200

    Implementierung

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { NgZone } from '@angular/core';
+import { ChangeDetectorRef, NgZone } from '@angular/core';
 import { Component } from '@angular/core';
 import Peer from 'peerjs';
+import { Participant, VotesPerItem } from './domain/domain';
 import { ParticipantStoreService } from './participant-store.service';
 
 @Component({
@@ -30,6 +31,7 @@ export class AppComponent {
 
   constructor(
     public fieldService: ParticipantStoreService,
+    private cdr: ChangeDetectorRef,
     private ngz: NgZone) {
   }
 
@@ -42,18 +44,26 @@ export class AppComponent {
   }
 
   public initNew(): void {
-    this.createPeer('admin');    
+    this.createPeer('admin', undefined);    
   }
 
   public joinAsVoter(): void {
-    this.createPeer('voter');    
+    this.joinAs('voter');    
   }
 
   public joinAsAdmin(): void {
-    this.createPeer('admin');    
+    this.joinAs('admin');    
   }
 
-  private createPeer(targetState: string) {
+  private joinAs(targetState: string) {
+    if (!this.formData.meetingID) {
+      alert('Bitte Meeting-ID angeben');
+    }
+    let peerId = this.formData.meetingID.substring(2);
+    this.createPeer(targetState, peerId);
+  }
+
+  private createPeer(targetState: string, idToJoin: string | undefined) {
     //var peer = new Peer(undefined, {host: 'localhost', port: 9000, key: 'peerjs', debug: 2});
     this.peer = new Peer(undefined, 
       {
@@ -69,11 +79,123 @@ export class AppComponent {
         console.log(err);
     });
     (this.peer as any).once('open', (id: string) => {
-        this.ngz.run(() => this.initParticipant(targetState));
+        this.ngz.run(() => this.initParticipant(targetState, idToJoin));
     });
   }
 
-  private initParticipant(targetState: string) {
+  private initParticipant(targetState: string, idToJoin: string | undefined) {
+    this.fieldService.init(new Participant(
+      this.peer, 
+      this.getOrGenerateName(),
+      true,
+      targetState === 'admin',
+      () => this.ngz.run(() => this.cdr.markForCheck())));
+    if (idToJoin) {
+      this.fieldService.participant.connectTo(idToJoin);
+    }
     this.state = targetState;
+  }
+
+  private getOrGenerateName(): string {
+    let paramValue = new URL(location.href).searchParams.get('user');
+    if (paramValue) {
+      return paramValue;
+    }
+    let name = window.localStorage.getItem('myUsername');
+    if (!name) {
+      name = Date.now().toString(36) + Math.round(Math.random() * 1000000).toString(36);
+      window.localStorage.setItem('myUsername', name);
+    }
+    return name;
+  }
+
+  public get participant() {
+    return this.fieldService.participant;
+  }
+
+  public getAdminLink(): string {
+    return this.getLink('a-');    
+  }
+
+  public getVoterLink(): string {
+    return this.getLink('v-');    
+  }
+
+  private getLink(prefix: string): string {
+    if (!this.peer) {
+      return '';
+    }
+    return window.location.href.split('?')[0] + "?distEstMeetingID=" + prefix + this.peer.id;
+  }
+
+  public reconnect() {
+    let id = prompt('Ziel-Peer-ID', this.formData.meetingID);
+    if (id) {
+      this.fieldService.participant.connectTo(id);
+    }  
+  }
+
+  public voteForTop() {
+    let topVote = this.getRadioList('topVoteForm', 'topVote');
+    let selection = topVote.value;
+    if (!selection) {
+      alert('Bitte wählen Sie einen Vorschlag aus!');
+      return;
+    }
+    this.participant.voteForTop(selection);
+  }
+
+  public saveEstimate() {
+    let type = this.getRadioList('estimationForm', 'estimationType');
+    let selection = type.value;
+    if (!selection) {
+      alert('Bitte wählen Sie einen Einschätzung aus!');
+      return;
+    }
+    let estimate = selection;
+    if (selection == 'Geld') {
+      let val = this.getSelectValue('estimationForm', 'moneyAmount');
+      if (!val) {
+        alert('Bitte wählen Sie einen Betrag aus!');
+        return;
+      }
+      estimate += ',' + val;
+    }
+    if (selection == 'Zeit') {
+      let val1 = this.getSelectValue('estimationForm', 'timeAmount');
+      if (!val1) {
+        alert('Bitte wählen Sie eine Dauer aus!');
+        return;
+      }
+      let val2 = this.getSelectValue('estimationForm', 'personAmount');
+      if (!val2) {
+        alert('Bitte wählen Sie eine Anzahl Personen aus!');
+        return;
+      }
+      estimate += ',' + val1 + ',' + val2;
+    }
+    this.participant.saveEstimate(estimate);
+    this.getRadioList('estimationForm', 'estimationType')
+      .forEach(item => (item as HTMLInputElement).checked = false);
+  }
+
+  private getSelectValue(formName: string, selectName: string): string {
+    let form = document.forms.namedItem(formName);
+    let sel = form?.elements.namedItem(selectName) as HTMLSelectElement;
+    return sel.value;
+  }
+
+  private getRadioList(formName: string, radioName: string) {
+    let form = document.forms.namedItem(formName);
+    return form?.elements.namedItem(radioName) as RadioNodeList;
+  }
+
+  public itemId(index: number, item: VotesPerItem) {
+    return item.id;
+  }
+
+  public activateEstimationType(value: string) {
+    let type = this.getRadioList('estimationForm', 'estimationType');
+    type.value = value;
   }
 }
--- src/app/domain/domain.ts
+++ src/app/domain/domain.ts
@@ -1,263 +1,306 @@
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 import { RTCDB } from 'rtcdb';
 
-interface IdeaDto {
-  title: string;
-  details: string;
+interface ItemDto {
+  text: string;
 }
 
-interface DeveloperDto {
-  name: string;
-  date: number;
-  color: string;
-  ideas: IdeaDto[];
+class VotesPerItem {
+  public topVoteCount: number = 0;
+  public totalEstimateCount: number = 0;
+  public finishedEstimateCount: number = 0;
+  private estimateMultiset: Map<string, number> = new Map();
+
+  constructor(
+    public readonly id: string, 
+    public readonly text: string) {
+
+  }
+
+  public countEstimate(estimate: string): void {
+    this.totalEstimateCount++;
+    if (estimate !== 'pending') {
+      this.finishedEstimateCount++;
+      if (this.estimateMultiset.has(estimate)) {
+        this.estimateMultiset.set(estimate, this.estimateMultiset.get(estimate) as number + 1);
+      } else {
+        this.estimateMultiset.set(estimate, 1);
+      }
+    }
 }
 
-  export interface MsgData {
-    to: string;
-    color: string;
-    tc: string;
-    tr: string;
+  public get pendingEstimateCount(): number {
+    return this.totalEstimateCount - this.finishedEstimateCount;
   }
 
-  class Developer {
-
-    public open: boolean = true;
-    private voterCache:Set<string>[] = [];
-    private cacheInvalid: boolean = true;
-    private lastMessage: string = "";
-    
-    constructor(
-      public readonly name: string,
-      private readonly db: RTCDB,
-      private readonly participant: Participant) {
-
-      this.db.on(['add', 'update'], 'votes', true, () => {
-        this.cacheInvalid = true;
-        this.participant.markCallback();
-      });
-      participant.registerMessageHandler((id: undefined, msg: any) => {
-        if (msg.to == this.name) {
-          this.lastMessage = msg.tc + ' ' + msg.tr;
-          if (this.lastMessage.length > 40) {
-            this.lastMessage = this.lastMessage.substring(0, 37) + '...';
-          }
-          this.participant.markCallback();
-        }
-      });
-    }
-
-    private get dto(): DeveloperDto {
-      return this.db.get('ideas', this.name) as DeveloperDto;
-    }
-
-    public get date(): number {
-      return this.dto.date;
-    }
-
-    public get ideas(): IdeaDto[] {
-      return this.dto.ideas;
-    }
-
-    public setOpen(o: boolean): void {
-      this.open = o;
-    }
-
-    voteCountFor(ideaIndex: number): number {
-      return this.votersFor(ideaIndex).size;
-    }
-
-    voterNamesFor(ideaIndex: number): string {
-      return Array.from(this.votersFor(ideaIndex)).join(', ');
-    }
-
-    private votersFor(ideaIndex: number): Set<string> {
-      if (this.cacheInvalid) {
-        this.voterCache = [];
-        this.participant.allParticipants().forEach(p => {
-          let vote = this.getVoteFrom(p);
-          if (typeof(vote) == 'number') {
-            if (!this.voterCache[vote]) {
-              this.voterCache[vote] = new Set();
-            }
-            this.voterCache[vote].add(p);
-          }
-        });
-        this.cacheInvalid = false;
-      }
-      if (this.voterCache[ideaIndex]) {
-        return this.voterCache[ideaIndex];
-      } else {
-        return new Set();
-      }
-    }
-
-    getVoteFrom(participantName: string): number|undefined {
-      return this.db.get('votes', this.name + '.from.' + participantName);
-    }
-
-    getTopIdea(): string {
-      this.votersFor(0);
-      let bestCnt = 0;
-      let best = "";
-      for (let i = 0; i < this.voterCache.length; i++) {
-        let voteCnt = this.voteCountFor(i);
-        if (voteCnt > bestCnt) {
-          bestCnt = voteCnt;
-          best = this.ideas[i].title;
-        }
-      }
-      return best;
-    }
-
-    getTopIdeaVoteCount(): number {
-      this.votersFor(0);
-      let bestCnt = 0;
-      for (let i = 0; i < this.voterCache.length; i++) {
-        let voteCnt = this.voteCountFor(i);
-        if (voteCnt > bestCnt) {
-          bestCnt = voteCnt;
-        }
-      }
-      return bestCnt;
-    }
-
-    getTotalVoteCount(): number {
-      this.votersFor(0);
-      let sum = 0;
-      for (let i = 0; i < this.voterCache.length; i++) {
-        sum += this.voteCountFor(i);
-      }
-      return sum;
-    }
-
-    getLastMessage(): string {
-      return this.lastMessage;
-    }
-
-    getLastMessageTime(): string {
-      let m = this.lastMessage.match(/[0-9][0-9]:[0-9][0-9]/);
-      if (m) {
-        return m[0];
-      } else {
-        return "";
-      }
-    }  
+  public get allEstimates(): string {
+    let keys = Array.from(this.estimateMultiset.keys());
+    keys.sort((a, b) => this.toNumber(a) - this.toNumber(b));
+    return keys.map(k => this.estimateMultiset.get(k) + 'x' + k).join('; ');
   }
-  
-  class Participant {
-    private readonly id: string;
-    public readonly name: string;
-    private readonly color: string;
-    private participants: string[] = [];
-    private developers: Developer[] = [];
-    private readonly db: RTCDB;
-    public readonly markCallback;
-    private sortedByMsg: boolean = false;
 
-    constructor(peer: any, ownId: string, ownName: string, ideas: IdeaDto[]|undefined, markCallback: Function) {
-      this.id = ownId;
-      this.name = ownName;
-      this.markCallback = markCallback;
-      let clean = typeof(ideas) != 'undefined';
-      this.db = new RTCDB('ideenmesse.' + ownName, peer, ownId, localStorage, clean);
-      this.db.on('add', 'ideas', true, (name: string, data: any) => {
-        this.participants.push(name);
-        if (data.ideas.length > 0) {
-          let dev = new Developer(name, this.db, this);
-          this.developers.push(dev);
-          this.ensureDevelopersSorted();
+  public get medianEstimate(): string {
+    let keys = Array.from(this.estimateMultiset.keys());
+    if (keys.length == 0) {
+      return '';
+    }
+    keys.sort((a, b) => this.toNumber(a) - this.toNumber(b));
+    let expanded: string[] = [];
+    keys.forEach(k => {
+      if (this.shallCount(k)) {
+        let cnt = this.estimateMultiset.get(k) as number;
+        for (let i = 0; i < cnt; i++) {
+          expanded.push(k);
         }
-        markCallback();
-      });
-
-      if (clean) {
-        let h = Math.floor(Math.random() * 72) * 5;
-        let s = 85 + Math.floor(Math.random() * 10);
-        let l = 35 + Math.floor(Math.random() * 10);
-        this.color = 'hsl(' + h + ',' + s + '%,' + l + '%)';
-        let me : DeveloperDto = {
-          name: ownName,
-          color: this.color,
-          date: new Date().getTime(),
-          ideas: ideas as IdeaDto[]
-        }
-        this.db.put('ideas', ownName, me);
-      } else {
-        this.color = (this.db.get('ideas', ownName) as DeveloperDto).color;
       }
+    });
+    if (expanded.length == 0) {
+      return '';
     }
-
-    getPeerID() {
-      return this.id;
-    }
-
-    registerMessageHandler(handler: Function) {
-        this.db.on('add', 'messages', true, handler);
-    }
-  
-    registerPlayerChangeHandler(handler: Function) {
-      this.db.on('add', 'currentPlayer', true, handler);
-      this.db.on('update', 'currentPlayer', false, handler);
-      this.db.on('add', 'playerData', true, handler);
-    }
-
-    connectToOtherPlayer(id: string) {
-      this.db.connectToNode(id);
-    }
-
-    public sortByMsg(b: boolean) {
-      this.sortedByMsg = b;
-      this.ensureDevelopersSorted();
-    }
-  
-    private ensureDevelopersSorted() {
-      if (this.sortedByMsg) {
-        this.developers.sort((a, b) => b.getLastMessageTime().localeCompare(a.getLastMessageTime()));
-      } else {
-        this.developers.sort((a, b) => a.date - b.date);
-      }
-    }
-
-    public allParticipants(): string[] {
-      return this.participants;
-    }
-
-    public allDevelopers(): Developer[] {
-      this.ensureDevelopersSorted();
-      return this.developers;
-    }
-
-    sendMessage(msg: string, to: string) {
-      this.sendMessageRaw(this.makeColored(to, curTime() + ' ' + this.name, msg));
-    }
-
-    makeColored(to: string, tc: string, tr: string): MsgData {
-      return {
-          color: this.color,
-          to: to,
-          tc: tc,
-          tr: tr
-      }
-    }
-
-    sendMessageRaw(msg: MsgData) {
-      this.db.add('messages', msg);
-    }    
-
-    votedFor(d: Developer): boolean {
-      return typeof(d.getVoteFrom(this.name)) == 'number';
-    }
-  
-    voteFor(d: Developer, ideaIdx: number): void {
-      this.db.put('votes', d.name + '.from.' + this.name, ideaIdx);
-    }
-  
+    return expanded[Math.floor(expanded.length / 2)];
   }
+
+  public get averageEstimate(): number {
+    let sum = 0;
+    let count = 0;
+    this.estimateMultiset.forEach((value, key) => {
+      if (this.shallCount(key)) {
+        sum += value * this.toNumber(key);
+        count += value;
+      }
+    });
+    return count == 0 ? 0 : sum / count;
+  }
+
+  public get relativeStandardDeviation(): number {
+    let avg = this.averageEstimate;
+    if (avg == 0) {
+      return 0;
+    }
+    let sum = 0;
+    let count = 0;
+    this.estimateMultiset.forEach((value, key) => {
+      if (this.shallCount(key)) {
+        let diff = this.toNumber(key) - avg;
+        sum += value * diff * diff;
+        count += value;
+      }
+    });
+    let stdDev = Math.sqrt(sum / count);
+    return stdDev * 100 / avg;
+  }
+
+  private toNumber(estimate: string): number {
+    let parts = estimate.split(',');
+    if (parts.length <= 1) {
+      return 0;
+    }
+    let product;
+    if (parts[0] == 'Geld') {
+      product = 1;
+    } else if (parts[0] == 'Zeit') {
+      product = 50;
+    } else {
+      product = 0;
+    }
+    for (let i = 1; i < parts.length; i++) {
+      product *= Number.parseFloat(parts[i]);
+    }
+    return product;
+  }
+
+  private shallCount(estimate: string): boolean {
+    return estimate != 'pending' && estimate != 'unknown';
+  }
+}
+
+class VoteSummary {
+  private items: Map<string, VotesPerItem> = new Map();
+
+  constructor(db: RTCDB) {
+    db.forEach('items', (id, dta) => {
+      let sid = id as string;      
+      this.items.set(sid, new VotesPerItem(sid, dta.text));
+    });
+    db.forEach('topVotes', (id, dta) => {
+      let itemVotes = this.items.get(dta);
+      if (itemVotes) {
+        itemVotes.topVoteCount++;
+      }
+    });
+    db.forEach('estimates', (id, dta) => {
+      let parts = (id as string).split('_for_');
+      let itemVotes = this.items.get(parts[1]);
+      if (itemVotes) {
+        itemVotes.countEstimate(dta);
+      }
+    });
+  }
+
+  public get sortedItems(): VotesPerItem[] {
+    let list: VotesPerItem[] = [];
+    this.items.forEach((value, key) => list.push(value));
+    list.sort((a, b) => {
+      let diff = b.topVoteCount - a.topVoteCount;
+      if (diff != 0) {
+        return diff;
+      }
+      return b.averageEstimate - a.averageEstimate;
+    });
+    return list;
+  }
+
+  public get stableItems(): VotesPerItem[] {
+    let list: VotesPerItem[] = [];
+    this.items.forEach((value, key) => list.push(value));
+    list.sort((a, b) => a.id.localeCompare(b.id));
+    return list;
+  }
+}
+  
+class Participant {
+  public readonly name: string;
+  private readonly db: RTCDB;
+  public readonly markCallback;
+
+  private currentItemId: string|undefined;
+  private cachedSummary: VoteSummary|undefined;
+
+  constructor(peer: any, ownName: string, clean: boolean, admin: boolean, markCallback: Function) {
+    this.name = ownName;
+    this.markCallback = markCallback;
+    this.db = new RTCDB('distEst.' + ownName, peer, clean);
+    this.db.on(['add', 'update'], 'items', false, () => this.invalidateCache());
+    this.db.on(['add', 'update'], 'topVotes', false, () => this.invalidateCache());
+    this.db.on(['add', 'update'], 'estimates', false, () => this.invalidateCache());
+
+    if (clean && admin) {
+      this.db.put('state', 'state', 'running');
+      this.db.put('items', 'uid1', {text: 'Idee 1: Eine tolle Idee'});
+      this.db.put('items', 'uid2', {text: 'Idee 2: Eine andere Idee'});
+      this.db.put('items', 'uid3', {text: 'Idee 3: Eine dritte Idee'});
+    }
+  }
+
+  private invalidateCache() {
+    this.cachedSummary = undefined;
+    this.markCallback();
+    console.log('invalidate cache');
+  }
+
+  public connectTo(idToJoin: string): void {
+    this.db.connectToNode(idToJoin);
+  }
+
+  public hasNoTopVote(): boolean {
+    return typeof(this.db.get('topVotes', this.name)) !== 'string';
+  }
+
+  public hasFurtherItems(): boolean {
+    return !!this.getCurrentItemId();
+  }
+
+  public getCurrentItemId(): string | undefined {
+    if (!this.currentItemId) {
+      this.determineNextItem();
+    }
+    return this.currentItemId;
+  }
+
+  public getCurrentItemText(): string {
+    let id = this.getCurrentItemId();
+    if (!id) {
+      return '';
+    }
+    let text = this.db.get('items', id)?.text;
+    return text ? text : '';
+  }
+
+  private determineNextItem() {
+    if (this.db.get('state', 'state') === 'ended' || this.hasNoTopVote()) {
+      console.log('ended');
+      this.currentItemId = undefined;
+      return;
+    }
+    let summary = this.voteSummary;
+    let notYetEstimated: VotesPerItem[] = [];
+    summary.stableItems.forEach(item => {
+      if (this.hasNotYetEstimated(item.id)) {
+        notYetEstimated.push(item);
+      }
+    });
+    if (notYetEstimated.length == 0) {
+      console.log('no more items');
+      this.currentItemId = undefined;
+      return;
+    }
+    if (notYetEstimated.length == 1) {
+      console.log('onyl one left');
+      this.startEstimating(notYetEstimated[0].id);
+      return;
+    }
+    console.log('not yet estimatd ' + notYetEstimated.map(x => x.id));
+    let c1 = this.pickRandom(notYetEstimated);
+    let c2 = this.pickRandom(notYetEstimated);
+    console.log('c1=' + c1.id);
+    console.log('c2=' + c2.id);
+    if (c1.finishedEstimateCount < c2.finishedEstimateCount
+      || (c1.finishedEstimateCount == c2.finishedEstimateCount && c1.totalEstimateCount < c2.totalEstimateCount)) {
+      console.log('pick one ' + c2.totalEstimateCount);
+      this.startEstimating(c1.id);
+    } else {
+      console.log('pick other' + c1.totalEstimateCount);
+      this.startEstimating(c2.id);
+    }
+  }
+
+  private hasNotYetEstimated(itemId: string): boolean {
+    let estimate = this.db.get('estimates', this.estimateKey(itemId));
+    console.log('estimate for ' + itemId + '=' + estimate);
+    return typeof(estimate) !== 'string' || estimate === 'pending';
+  }
+
+  private pickRandom(list: VotesPerItem[]): VotesPerItem {
+    let rnd = Math.random();
+    return list[Math.floor(rnd * list.length)];
+  }
+
+  private startEstimating(itemId: string) {
+    this.currentItemId = itemId;
+    this.setEstimateRaw(this.currentItemId, 'pending');
+  }
+
+  public saveEstimate(estimate: string) {
+    if (!this.currentItemId) {
+      return;
+    }
+    this.setEstimateRaw(this.currentItemId, estimate);
+    this.currentItemId = undefined;
+  }
+
+  private setEstimateRaw(itemId: string, estimate: string) {
+    this.db.put('estimates', this.estimateKey(itemId), estimate);
+  }
+
+  private estimateKey(itemId: string) {
+    return this.name + '_for_' + itemId;
+  }
+
+  public get voteSummary(): VoteSummary {
+    if (!this.cachedSummary) {
+      this.cachedSummary = new VoteSummary(this.db);
+    }
+    return this.cachedSummary;
+  }
+
+  voteForTop(itemId: string) {
+    this.startEstimating(itemId);
+    this.db.put('topVotes', this.name, itemId);
+    this.invalidateCache();
+  }
+}
 
 function curTime(): string {
   return new Date().toLocaleTimeString('de-DE', {hour: '2-digit', minute:'2-digit'});
 }
 
-export { Participant };
+export { Participant, VotesPerItem };
