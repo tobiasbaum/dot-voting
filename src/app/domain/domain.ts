accd6647ac75bb482e868b5d57a0f5f91cac5338
Tobias Baum <tbaum@tntinteractive.de>
2021-05-08 17:15:48 +0200

    Vergabe doppelter Item-IDs verhindern
    (cherry picked from commit 2b88f2305d9fcb608ec942dd89f6c74f70b23fcc)

--- src/app/domain/domain.ts
+++ src/app/domain/domain.ts
@@ -205,8 +205,6 @@ class Participant {
   }
 
   public addItem(trimmed: string): void {
-    let key = this.name + this.addedItemCount;
-    this.addedItemCount++;
     let obj : ItemDto;
     let colonIndex = trimmed.indexOf(':');
     if (colonIndex >= 0) {
@@ -220,6 +218,11 @@ class Participant {
         text: trimmed
       };
     }
+    let key;
+    do {
+      key = this.name + this.addedItemCount;
+      this.addedItemCount++;
+    } while (this.db.get('items', key))
     this.db.put('items', key, obj);
   }
 
